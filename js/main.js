bb8107c07635775b749f63922eb624f96148d40b
Rodrigo Fuentes <fuenro23@gmail.com>
2020-09-09 14:59:46 -0500

    Terminando el slider, agregando la funcinalidad del boton izquierdo y dando estilos a pequeños detalles

--- js/main.js
+++ js/main.js
@@ -39,16 +39,33 @@ let right = document.querySelector('#btn-right'),
 	left = document.querySelector('#btn-left'),
 	slider = document.querySelector('#slider');
 
-
-
+let aux2=1;
 let aux = 1;
-right.addEventListener('click',() =>{
-	slider.setAttribute('src',img[aux]);
-	aux++;
-	if (aux == img.length) {
-		aux=0;
-	}
-});
+function sliderButtons() {
+	right.addEventListener('click',() =>{
+		slider.setAttribute('src',img[aux]);
+		aux++;
+		console.log('control derecho'+aux)
+		aux2 = aux;
+		if (aux == img.length) {
+			aux=0;
+			aux2 = img.length;
+		}
+		console.log('control izquierdo : '+aux2);
+	});
+	left.addEventListener('click',() =>{
+		aux2--;
+		console.log('control izquierdo :'+aux2);
+		slider.setAttribute('src',img[aux2]);
+		aux = aux2;
+		if (aux2 == 0) {
+			aux = aux2;
+			aux2= img.length;
+		}
+		console.log('control derecho'+aux)
+	});
+}
 
 
+sliderButtons();
 
