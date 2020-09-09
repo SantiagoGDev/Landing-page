7922f65b0256d5487965b2f333a6d20959dfc0b4
Rodrigo Fuentes <fuenro23@gmail.com>
2020-09-09 02:12:35 -0500

    re maquetando el modal para convertirlo en modal dando  estilos al slider con css y funcionalidad con JS  agrengando 8 img para slider

--- js/main.js
+++ js/main.js
@@ -33,3 +33,22 @@ window.addEventListener('click', e =>{
 
 	}
 })
+
+let img = ['img/slider-1.png','img/slider-2.jpg','img/slider-3.png','img/slider-4.png','img/slider-5.png','img/slider-6.png','img/slider-7.png','img/slider-8.png'];
+let right = document.querySelector('#btn-right'),
+	left = document.querySelector('#btn-left'),
+	slider = document.querySelector('#slider');
+
+
+
+let aux = 1;
+right.addEventListener('click',() =>{
+	slider.setAttribute('src',img[aux]);
+	aux++;
+	if (aux == img.length) {
+		aux=0;
+	}
+});
+
+
+
